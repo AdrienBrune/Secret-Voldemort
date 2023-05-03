9a66f3fce6c5a496449d54916ae1a3015c2f9ae0
Brune Adrien <brune.adrien.50@gmail.com>
2023-05-03 21:24:05 +0200

    Update easel animation

--- SV_GAME/Src/Widgets/Components/w_easel.cpp
+++ SV_GAME/Src/Widgets/Components/w_easel.cpp
@@ -1,16 +1,47 @@
 #include "w_easel.h"
 #include "Handlers/c_playershandler.h"
 
+#include <QTimer>
+
 W_Easel::W_Easel(QWidget *parent)
     : QWidget(parent)
+    , mHover(false)
 {
     setSizePolicy(QSizePolicy::MinimumExpanding, QSizePolicy::MinimumExpanding);
     show();
+
+    QTimer *timer = new QTimer(this);
+    connect(timer, &QTimer::timeout, this, &W_Easel::onPositionChanged);
+    timer->start(5000);
 }
 
 W_Easel::~W_Easel()
 {
 
+}
+
+void W_Easel::onPositionChanged()
+{
+    if(!C_PlayersHandler::getInstance())
+        return;
+
+    if(!C_PlayersHandler::getInstance()->getMyPlayerInstance())
+        return;
+
+    switch(C_PlayersHandler::getInstance()->getMyPlayerInstance()->getPosition())
+    {
+        case C_Player::E_POSITION::Director:
+            setToolTip("Vous êtes Directeur de Poudlard\nVous voterez une loi parmis les deux proposées par le Ministre de la magie");
+            break;
+
+        case C_Player::E_POSITION::Minister:
+            setToolTip("Vous êtes le Ministre de la magie\nPiochez trois lois et n'en faites parvenir que deux au Directeur de Poudlard\nLe déclenchement d'un pouvoir devra être utilisé par vous");
+            break;
+
+        default:
+            setToolTip("");
+            break;
+    }
 }
 
 void W_Easel::paintEvent(QPaintEvent *)
@@ -27,9 +58,16 @@ void W_Easel::paintEvent(QPaintEvent *)
 
     double size = 0, xWidth = width()-2*MARGIN_LEFT_POURCENT*width(), yHeight = height()-2*MARGIN_TOP_POURCENT*height();
     if(xWidth < yHeight*RATIO_EASEL)
+    {
         size = xWidth;
+    }
     else
+    {
         size = yHeight*RATIO_EASEL;
+    }
+    if(!mHover)
+        size = size*0.95;
+
     QRect area(MARGIN_LEFT_POURCENT*width() + (xWidth - size)/2, MARGIN_TOP_POURCENT*height() + (yHeight - size/RATIO_EASEL)/2, size, size/RATIO_EASEL);
     C_Player::E_POSITION position = C_PlayersHandler::getInstance()->getMyPlayerInstance()->getPosition();
 
--- SV_GAME/Src/Widgets/Components/w_easel.h
+++ SV_GAME/Src/Widgets/Components/w_easel.h
@@ -3,6 +3,7 @@
 
 #include <QWidget>
 #include <QPainter>
+#include <Handlers/c_playershandler.h>
 
 class W_Easel : public QWidget
 {
@@ -12,8 +13,16 @@ public:
     explicit W_Easel(QWidget *parent = nullptr);
     ~W_Easel();
 
+public slots:
+    void onPositionChanged();
+
 protected:
+    void leaveEvent(QEvent*)override{ mHover = false; update(); };
+    void enterEvent(QEvent*)override{ mHover = true; update(); };
     void paintEvent(QPaintEvent *event)override;
+
+private:
+    bool mHover;
 };
 
 #endif // W_EASEL_H
