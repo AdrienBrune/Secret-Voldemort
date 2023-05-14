95d82e63d461a85579e58b00cff27e50ba0760d8
Brune Adrien <brune.adrien.50@gmail.com>
2023-05-14 15:48:57 +0200

    Release MacOS

--- SV_GAME/Release/releaseMacOS.sh
+++ SV_GAME/Release/releaseMacOS.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
 
-if [ test -f "/MacOS/SV_Game.dmg" ]
-then
-	echo Impossible to release. Remove dmg file before releasing ...
-	sleep 4
-else
-	if [ ~/Applications/Qt/5.15.2/clang_64/bin/macdeployqt ./MacOS/SV_Game.app -dmg ]
-	then
-		mv ./MacOS/SV_Game.dmg ./
-		rm -r ./MacOS
-		mkdir ./MacOS
-		mv SV_Game.dmg ./MacOS
-		echo Release success ...
-		sleep 4
-	else
-		echo macdeployqt command failed ...
-		sleep 4
-	fi
-fi
+~/Applications/Qt/5.15.2/clang_64/bin/macdeployqt ./MacOS/SV_Game.app -dmg
+mv ./MacOS/SV_Game.dmg ./
+rm -r ./MacOS
+mkdir ./MacOS
+mv SV_Game.dmg ./MacOS
--- SV_SERVER/Release/releaseMacOS.sh
+++ SV_SERVER/Release/releaseMacOS.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
 
-if [ test -f "/MacOS/SV_Server.dmg" ]
-then
-	echo Impossible to release. Remove dmg file before releasing ...
-	sleep 4
-else
-	if [ ~/Applications/Qt/5.15.2/clang_64/bin/macdeployqt ./MacOS/SV_Server.app -dmg ]
-	then
-		mv ./MacOS/SV_Server.dmg ./
-		rm -r ./MacOS
-		mkdir ./MacOS
-		mv SV_Server.dmg ./MacOS
-		echo Release success ...
-		sleep 4
-	else
-		echo macdeployqt command failed ...
-		sleep 4
-	fi
-fi
+~/Applications/Qt/5.15.2/clang_64/bin/macdeployqt ./MacOS/SV_Server.app -dmg
+mv ./MacOS/SV_Server.dmg ./
+rm -r ./MacOS
+mkdir ./MacOS
+mv SV_Server.dmg ./MacOS
